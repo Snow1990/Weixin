e40650206e520546d89e5b34cc34677aceb7f025
Snow1990 <snn@shaonandeMacBook-Pro.local>
2015-06-01 16:50:50 +0800

    添加常量类

---
+++ Weixin/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Weixin
+//
+//  Created by SN on 15/6/1.
+//  Copyright (c) 2015年 Snow. All rights reserved.
+//
+
+import Foundation
+
+class Constants{
+    static let ToLoginSegue = "ToLoginSegue"
+    
+    
+}
