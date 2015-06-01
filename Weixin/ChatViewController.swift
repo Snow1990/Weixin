675f9c40828d9043b95a9d53a21cdc388ab2d6e9
Snow1990 <snn@shaonandeMacBook-Pro.local>
2015-06-01 16:45:41 +0800

    用户界面搭建

---
+++ Weixin/ChatViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ChatViewController.swift
+//  Weixin
+//
+//  Created by SN on 15/6/1.
+//  Copyright (c) 2015年 Snow. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    @IBAction func send(sender: UIBarButtonItem) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
