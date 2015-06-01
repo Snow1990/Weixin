c9ca09878858e889cafb1568193eda6435e12dec
Snow1990 <snn@shaonandeMacBook-Pro.local>
2015-06-01 18:47:52 +0800

    配置XMPP框架

---
+++ wxxmppHeader.h
@@ -0,0 +1,16 @@
+//
+//  wxxmppHeader.h
+//  Weixin
+//
+//  Created by SN on 15/6/1.
+//  Copyright (c) 2015年 Snow. All rights reserved.
+//
+
+#ifndef Weixin_wxxmppHeader_h
+#define Weixin_wxxmppHeader_h
+
+
+#endif
+
+
+#import <XMPP.h>
